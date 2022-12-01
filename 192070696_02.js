9a3af8c239aff4e9309a2a034b70de3f772e7e50
Juan08302001 <92490482+Juan08302001@users.noreply.github.com>
2022-11-30 18:47:52 -0600

    Add files via upload

---
+++ 192070696_02.js
@@ -0,0 +1,22 @@
+/*En este ejercicio se le va a pedir escribir una frase de al menos 10 palabras y que se "separen"
+cada una de las palabras y se impriman los elementos.*/
+
+
+let palabra = 'Hola mundo desde NodeJs con la fecha 30 11 2022';
+let palabra2 = palabra.split(",");
+console.log("Palabra normal:", palabra);
+/*console.log(palabra2);*/
+
+let palabra3 = palabra.split(" ");
+
+console.log("Palabras separadas:")
+console.log(palabra3[0])
+console.log(palabra3[1])
+console.log(palabra3[2])
+console.log(palabra3[3])
+console.log(palabra3[4])
+console.log(palabra3[5])
+console.log(palabra3[6])
+console.log(palabra3[7])
+console.log(palabra3[8])
+console.log(palabra3[9])
